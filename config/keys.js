dd01866d890e409b12769554c6d3356fb8e076bd
Alex J.Y <jinyiabc@gmail.com>
2018-02-16 23:13:24 +0800

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+          clientID: '626437744936-ft7h370702g5ospsgjaiol6icu58r97r.apps.googleusercontent.com',
+          clientSecret: 'VWGggMGoa0Yt62W6H604xEcr'
+        },
+        mongodb: {
+            dbURI: 'mongodb://heroku_r77gkqv7:6cp5o8ggkvpqt3pfq8391jn0vi@ds239128.mlab.com:39128/heroku_r77gkqv7'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
